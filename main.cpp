caf9980ef885281d85963c20ba5c28fa2bbe0ddc
kapo <hktaipolankapo@gmail.com>
2022-01-13 14:14:29 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Hollow World
+    //std::
+   // cout << "Hello, World!\n";
+   // cout << "Hi, World!\n";
+   // return 0;
+    
+    //Pointer
+    int number = 5 ;
+    int* numberPtr = &number ;
+    cout << &number << endl ;
+    cout << *numberPtr << endl ;
+    
+    
+}
+
+
