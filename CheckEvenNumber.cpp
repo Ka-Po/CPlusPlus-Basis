20591613088035bf6c63f410263e2e26f4afd39f
Ka Po <74723650+Ka-Po@users.noreply.github.com>
2023-04-26 07:26:17 +0800

    Update CheckEvenNumber.cpp

--- CheckEvenNumber.cpp
+++ CheckEvenNumber.cpp
@@ -8,12 +8,12 @@ int main() {
     cin >> number;
     
     bool isEvenNumber = false;
-    for (int i = 2; i < number; i++) {              // 這個時候的for回圈是没有用，但如果改成要Check一個範圍的值，就會用到for回圈。
-        if (number % i == 0) {
-            isEvenNumber = true ;
-            break;
-        }
+    
+    if (number % i == 0) {
+        isEvenNumber = true ;
+        break;
     }
+    
     
     if (isEvenNumber == true) {
         cout << "Even Number"<<endl;
