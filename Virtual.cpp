9bc38a345b2cbcc613a6872a9bf456a71006adb4
Ka Po <74723650+Ka-Po@users.noreply.github.com>
2023-04-26 07:18:49 +0800

    Update Virtual.cpp

--- Virtual.cpp
+++ Virtual.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class OS{
 public:
-    virtual void CanTyping()=0;                 // 要等於 0 ，佢先係模版。如果乜都冇或者用括號括住，佢的被繼承層，唔需要有同名的方法。
+    virtual void CanTyping()=0;                 // 要等於 0 ，才是模版。如果没有0或者用括號括起來，佢的被繼承層，不需要有同名的方法。
 //    {
 //        cout << "OS can Typing!\n";
 //    };
